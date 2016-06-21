4e15664e1cb1cf86152f924d5e471ee806a43ec6
James Bubb <=>
2016-06-21 15:44:10 +0100

    Un-wordpressify.

--- index.php
+++ index.php
@@ -1,61 +1,144 @@
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link http://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link href='https://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <section class="jumbotron">
+    <div class="container">
+      <header>
 
-get_header(); ?>
+        <h1>James Bubb </h1>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+        <div class="social">
+          <a href="https://www.linkedin.com/in/james-bubb-40753142"><img src="<?php echo get_template_directory_uri(); ?>/img/sm/Linkedin.png" alt=""></a>
+          <a href="https://github.com/codebubb"><img src="<?php echo get_template_directory_uri(); ?>/img/sm/Github.png" alt=""></a>
+          <a href="https://twitter.com/codebubb"><img src="<?php echo get_template_directory_uri(); ?>/img/sm/Twitter.png" alt=""></a>
+        </div>
+      </header>
+    </div>
+  </section>
 
-		<?php if ( have_posts() ) : ?>
+  <section class="info">
+    <div class="container">
+      <h2>Full stack developer</h2>
+      <h3 style="text-align: center;">Hi, i'm James and I do stuff with the web.</h3>
+      <p>
+        I've always been interested in coding and design but it wasn't until last year that I started <a href="">FreeCodeCamp</a> that my passion for development was ignited. Since then i've worked hard to develop a robust set of <a href="#skills">skills</a> that allow me to work both front and back end giving me flexbility to develop full-stack applications.
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
+        I have previously worked for a successfull web hosting company which gifted me with the technical abilities of server administration.</p>
+        <br>
+        <h4 style="text-align: center;">I am 100% dedicated to learning more and improving skills around web dev.</h4>
+      </div>
+    </section>
 
-			<?php
-			// Start the loop.
-			while ( have_posts() ) : the_post();
+    <section class="page" id="skills">
+      <div class="container">
+        <h2>Skills</h2>
+        <h3 style="text-align: center;">These are some of the technologies I feel proficient using.</h3>
+        <ul>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/img/html5.png" alt=""></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/img/css.png" alt=""></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/img/js.png" alt=""></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/img/jquery.gif" alt=""></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/img/angular.png" alt=""></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/img/git.png" alt=""></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/img/photoshop.png" alt=""></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/img/nodejs.png" alt=""></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/img/expressjs.png" alt=""></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/img/mongodb.jpeg" alt=""></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/img/php.png" alt=""></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/img/mysql.png" alt=""></li>
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+        </ul>
+      </div>
+    </section>
+    <section id="projects">
+      <div class="container">
+        <h2>Projects</h2>
+        <ul>
+          <li>
+            <div class="project">
+              <img src="" alt="">
+              <p>Angular Project</p>
+            </div>
+          </li>
+          <li>
+            <div class="project">
+              <img src="" alt="">
+              <p>Online Bingo Fans</p>
+            </div>
+          </li>
+          <li>
+            <div class="project">
+              <img src="" alt="">
+              <p>Quick API</p>
+            </div>
+          </li>
+          <li>
+            <div class="project">
+              <img src="" alt="">
+              <p>SQL Tutor</p>
+            </div>
+          </li>
+          <li>
+            <div class="project">
+              <img src="" alt="">
+              <p>SQL Injection</p>
+            </div>
+          </li>
+          <li>
+            <div class="project">
+              <img src="" alt="">
+              <p>Code Course</p>
+            </div>
+          </li>
+          <li>
+            <div class="project">
+              <img src="" alt="">
+              <p>Jobs Board</p>
+            </div>
+          </li>
+          <li>
+            <div class="project">
+              <img src="" alt="">
+              <p>JS Fundamentals</p>
+            </div>
+          </li>
+        </ul>
+        <h3 style="text-align: center;">For more examples take a look at my <a href="">GitHub</a> account or work on <a href="">CodePen</a></h3>
+      </section>
+    </div>
+    <section id="blog">
+      <div class="container">
+				<h2>Blog</h2>
+				<?php
+	// Start the loop.
+	while ( have_posts() ) : the_post();
+		?><h3><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
+		<?php
+		// the_content();
+		// Include the page content template.
+		// get_template_part( 'template-parts/content', 'page' );
 
-			// End the loop.
-			endwhile;
+		// If comments are open or we have at least one comment, load up the comment template.
+		if ( comments_open() || get_comments_number() ) {
+			comments_template();
+		}
 
-			// Previous/next page navigation.
-			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentysixteen' ),
-				'next_text'          => __( 'Next page', 'twentysixteen' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>',
-			) );
-
-		// If no content, include the "No posts found" template.
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
-
-<?php get_footer(); ?>
+		// End of the loop.
+	endwhile;
+	?>
+			</div>
+      </section>
+    <footer>
+      <div class="container">
+        <p>&copy; James Bubb 2016</p>
+      </div>
+    </footer>
+  </body>
+  </html>
--- single.php
+++ single.php
@@ -1,54 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link href='https://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<section id="blog">
+  <div class="container">
+    <?php
+// Start the loop.
+while ( have_posts() ) : the_post();
+?><h1><?php the_title();?></h1>
 <?php
-/**
- * The template for displaying all single posts and attachments
- *
- * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
- */
+the_content();
+// Include the page content template.
+// get_template_part( 'template-parts/content', 'page' );
 
-get_header(); ?>
+// If comments are open or we have at least one comment, load up the comment template.
+if ( comments_open() || get_comments_number() ) {
+  comments_template();
+}
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
-		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
-
-			// Include the single post content template.
-			get_template_part( 'template-parts/content', 'single' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) {
-				comments_template();
-			}
-
-			if ( is_singular( 'attachment' ) ) {
-				// Parent post navigation.
-				the_post_navigation( array(
-					'prev_text' => _x( '<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'twentysixteen' ),
-				) );
-			} elseif ( is_singular( 'post' ) ) {
-				// Previous/next post navigation.
-				the_post_navigation( array(
-					'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentysixteen' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Next post:', 'twentysixteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-					'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentysixteen' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Previous post:', 'twentysixteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-				) );
-			}
-
-			// End of the loop.
-		endwhile;
-		?>
-
-	</main><!-- .site-main -->
-
-	<?php get_sidebar( 'content-bottom' ); ?>
-
-</div><!-- .content-area -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+// End of the loop.
+endwhile;
+?>
+  </div>
+  </section>
+  <footer>
+    <div class="container">
+      <p>&copy; James Bubb 2016</p>
+    </div>
+  </footer>
+</body>
+</html>
