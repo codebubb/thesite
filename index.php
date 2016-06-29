660f9843a46c70003df376e94acca3f8b3463d6c
James Bubb <jpbubb82@gmail.com>
2016-06-29 16:16:14 +0100

    Add CSS flip to projects

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <title>James Bubb | Does stuff with the web</title>
   <link href='https://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Droid+Serif' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/animate.css">
 
@@ -33,21 +34,21 @@
 
   <section id="info" class="info">
     <div class="container">
-			<h2 style="text-align: center;">Hi, i'm James and I do stuff with the web.</h2>
+			<h2 style="text-align: center;">Hi, i'm James and I do stuff with the web.<hr></h2>
 
       <p>
         I've always been interested in coding and design but it wasn't until last year that I started <a href="">FreeCodeCamp</a> that my passion for development was ignited. Since then i've worked hard to develop a robust set of <a href="#skills">skills</a> that allow me to work both front and back end giving me flexbility to develop full-stack applications.
 
         I have previously worked for a successfull web hosting company which gifted me with the technical abilities of server administration.</p>
-        <br>
-        <h4 style="text-align: center;">I am 100% dedicated to learning more and improving skills around web dev.</h4>
+        <h3 style="text-align: center;">I am 100% dedicated to learning more and improving skills around web dev.</h3>
       </div>
     </section>
 
     <section class="page" id="skills">
       <div class="container">
-        <h2>Skills</h2>
-        <h3 style="text-align: center;">These are some of the technologies I feel proficient using.</h3>
+        <h2>Skills<hr></h2>
+
+        <h3 style="text-align: center;">These are some technologies I feel pretty confident using.</h3>
         <ul>
           <li><img src="<?php echo get_template_directory_uri(); ?>/img/html5.png" alt=""></li>
           <li><img src="<?php echo get_template_directory_uri(); ?>/img/css.png" alt=""></li>
@@ -56,65 +57,119 @@
           <li><img src="<?php echo get_template_directory_uri(); ?>/img/nodejs.png" alt=""></li>
           <li><img src="<?php echo get_template_directory_uri(); ?>/img/git.png" alt=""></li>
         </li>
-        <h3>These are some things I have some experience with.</h3>
+        <h3>And these are some I have experience with (but i'm not an expert).</h3>
         <ul>
           <li><img src="<?php echo get_template_directory_uri(); ?>/img/angular.png" alt=""></li>
           <li><img src="<?php echo get_template_directory_uri(); ?>/img/photoshop.png" alt=""></li>
           <li><img src="<?php echo get_template_directory_uri(); ?>/img/expressjs.png" alt=""></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/img/rails.png" alt=""></li>
           <li><img src="<?php echo get_template_directory_uri(); ?>/img/mongodb.jpeg" alt=""></li>
           <li><img src="<?php echo get_template_directory_uri(); ?>/img/php.png" alt=""></li>
           <li><img src="<?php echo get_template_directory_uri(); ?>/img/mysql.png" alt=""></li>
         </li>
         </ul>
-        <h3>And these are some things i'm currently learning.</h3>
+        <h3>Finally, this is what i'm currently learning.</h3>
         <ul>
           <li><img src="<?php echo get_template_directory_uri(); ?>/img/android.png" alt=""></li>
+          <li><img src="<?php echo get_template_directory_uri(); ?>/img/react.png" alt=""></li>
           <li><img src="<?php echo get_template_directory_uri(); ?>/img/gulp.png" alt=""></li>
         </ul>
       </div>
     </section>
     <section id="projects">
+      <!-- Thanks to https://davidwalsh.name/css-flip for flip effect -->
       <div class="container">
         <h2>Projects</h2>
         <ul>
           <li>
-            <div class="project">
-              <img src="" alt="">
-              <p>Angular Project</p>
+            <h3>Dad belly</h3>
+            <div class="project-container">
+              <div class="flipper">
+                <div class="front">
+                  <img src="<?php echo get_template_directory_uri(); ?>/img/projects/dadbelly.png" alt="">
+                </div>
+                <div class="back">
+                </div>
+              </div>
             </div>
           </li>
           <li>
-            <div class="project">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/projects/onlinebingofans.png" alt="">
-              <p>Online Bingo Fans</p>
+            <h3>Online Bingo Fans</h3>
+            <div class="project-container">
+              <div class="flipper">
+                <div class="front">
+                  <img src="<?php echo get_template_directory_uri(); ?>/img/projects/onlinebingofans.png" alt="">
+                </div>
+                <div class="back">
+                  <p>
+                    Bingo fans every will be delighted!
+                  </p>
+                </div>
+              </div>
             </div>
           </li>
           <li>
-            <div class="project">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/projects/quickapi.png" alt="">
-              <p>Quick API</p>
+            <h3>Quick API</h3>
+            <div class="project-container">
+              <div class="flipper">
+                <div class="front">
+                  <img src="<?php echo get_template_directory_uri(); ?>/img/projects/quickapi.png" alt="">
+                </div>
+                <div class="back">
+                  <p>
+                    A quick api
+                  </p>
+                </div>
+              </div>
             </div>
           </li>
           <li>
-            <div class="project">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/projects/sqlinjection.png" alt="">
-              <p>SQL Injection</p>
+            <h3>SQL Injection Challenge</h3>
+            <div class="project-container">
+              <div class="flipper">
+                <div class="front">
+                  <img src="<?php echo get_template_directory_uri(); ?>/img/projects/sqlinjection.png" alt="">
+                </div>
+                <div class="back">
+                  <p>
+                    SQL Injection
+                  </p>
+                </div>
+              </div>
             </div>
           </li>
           <li>
-            <div class="project">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/projects/itjobs.png" alt="">
-              <p>Jobs Board</p>
+            <h3>IT Jobs Board</h3>
+            <div class="project-container">
+              <div class="flipper">
+                <div class="front">
+                  <img src="<?php echo get_template_directory_uri(); ?>/img/projects/itjobs.png" alt="">
+                </div>
+                <div class="back">
+                  <p>
+                    An IT Jobs board
+                  </p>
+                </div>
+              </div>
             </div>
           </li>
           <li>
-            <div class="project">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/projects/jsfundamentals.png" alt="">
-              <p>JS Fundamentals</p>
+            <h3>JS Fundamentals</h3>
+            <div class="project-container">
+              <div class="flipper">
+                <div class="front">
+                  <img src="<?php echo get_template_directory_uri(); ?>/img/projects/jsfundamentals.png" alt="">
+                </div>
+                <div class="back">
+                  <p>
+                    JS Fundamentals
+                  </p>
+                </div>
+              </div>
             </div>
           </li>
         </ul>
-        <h3 style="text-align: center;">For more examples take a look at my <a href="">GitHub</a> account or work on <a href="">CodePen</a></h3>
+        <h3 style="text-align: center;">For more examples take a look at my <a href="">GitHub</a> account.</h3>
       </section>
     </div>
     <section id="blog">
@@ -136,7 +191,8 @@
       </section>
     <footer>
       <div class="container">
-        <p>&copy; James Bubb 2016</p>
+        <p>Android logo courtesy of Google Inc. React logo courtesty of Facebook</p>
+        <p>All other work &copy; James Bubb 2016</p>
       </div>
     </footer>
     <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.min.js"></script>
@@ -153,7 +209,7 @@
         e.preventDefault();
         $('html, body').animate({scrollTop: $(this.getAttribute('href')).offset().top }, 1000);
       });
-  
+
 
     });
 
