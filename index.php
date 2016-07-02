f9c861ed67fea40b3af264bc342a4aee58ebfc23
James Bubb <jpbubb82@gmail.com>
2016-07-02 23:08:52 +0100

    Tidy up copy

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>James Bubb | Does stuff with the web</title>
-  <link href='https://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'>
+  <!-- <link href='https://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'> -->
 <link href='https://fonts.googleapis.com/css?family=Droid+Serif' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/animate.css">
@@ -37,28 +37,28 @@
 			<h2 style="text-align: center;">Hi, i'm James and I do stuff with the web.<hr></h2>
 
       <p>
-        I've always been interested in coding and design but it wasn't until last year that I started <a href="">FreeCodeCamp</a> that my passion for development was ignited. Since then i've worked hard to develop a robust set of <a href="#skills">skills</a> that allow me to work both front and back end giving me flexbility to develop full-stack applications.
+        I've always been interested in coding but it wasn't until last year when I started <a href="http://freecodecamp.com/codebubb">FreeCodeCamp</a> that my passion for development was ignited. Since then i've worked hard to develop a robust set of <a href="#skills">skills</a> that allow me to work both front and back end giving me flexbility to develop full-stack applications.
 
-        I have previously worked for a successfull web hosting company which gifted me with the technical abilities of server administration.</p>
+        In my current role I work with apprentices, teaching them the the fundamenals of programming.  I have also previously worked within a successful support team for a professional web hosting company which allowed me to develop a keen problem solving mindset.</p>
         <br>
-        <h3 style="text-align: center;">I am 100% dedicated to learning more and improving skills around web dev.</h3>
+        <h3 >I am 100% dedicated to developing new skills, perfecting my talents and becoming a top-class full-stack developer.</h3>
       </div>
     </section>
 
     <section class="page" id="skills">
       <div class="container">
         <h2>Skills<hr></h2>
-
+        <!-- Thanks to https://davidwalsh.name/css-flip for flip effect -->
         <h3 style="text-align: center;">These are some technologies I feel pretty confident using.</h3>
         <ul class="skill-list">
           <li>
             <div class="skill-container">
               <div class="flipper">
                 <div class="front">
-                  <img src="<?php echo get_template_directory_uri(); ?>/img/html5.png" alt="">
+                  <img src="<?php echo get_template_directory_uri(); ?>/img/html5.png" alt="HTML5">
                 </div>
                 <div class="back">
-                  <p>I usually sketch out the content for a page before applying any style.  I sometimes use HAML.</p>
+                  <p>I think it's important to have good semantic HTML. I would like to learn more about accessibility. I sometimes use <a href="http://haml.info">HAML</a>.</p>
                 </div>
               </div>
             </div>
@@ -70,7 +70,7 @@
                   <img src="<?php echo get_template_directory_uri(); ?>/img/css3.png" alt="">
                 </div>
                 <div class="back">
-                  <p>I like to use SASS to pre-process CSS markup but have recently been writing specific selectors from scratch.</p>
+                  <p>I like to use SASS to pre-process CSS but have recently been writing selectors independently to better understand the cascade.</p>
                 </div>
               </div>
             </div>
@@ -94,7 +94,7 @@
                   <img src="<?php echo get_template_directory_uri(); ?>/img/bootstrap.png" alt="">
                 </div>
                 <div class="back">
-                  <p>BootStrap is great for quickly adding style to forms, buttons etc. I find the collapisble navs great for mobile!</p>
+                  <p>BootStrap is great for quickly adding style to forms, buttons etc. I find the collapsible navs great for mobile!</p>
                 </div>
               </div>
             </div>
@@ -106,7 +106,7 @@
                   <img src="<?php echo get_template_directory_uri(); ?>/img/jquery.png" alt="">
                 </div>
                 <div class="back">
-                  <p>When a project uses one or more features that jQuery provides, I usually include it. </p>
+                  <p>I like the way jQuery makes XHR easy to implement. I try and avoid using jQuery in a projec if it is just for CSS selectors.</p>
                 </div>
               </div>
             </div>
@@ -118,7 +118,7 @@
                   <img src="<?php echo get_template_directory_uri(); ?>/img/nodejs.png" alt="">
                 </div>
                 <div class="back">
-                  <p>I like the provided packages and wealth of re-usable code provided by npm.</p>
+                  <p>Node's async nature took a bit of getting used to but I feel it can be a really powerful way of handling server side tasks.</p>
                 </div>
               </div>
             </div>
@@ -130,7 +130,7 @@
                   <img src="<?php echo get_template_directory_uri(); ?>/img/git.png" alt="">
                 </div>
                 <div class="back">
-                  <p>I use BitBucket to hold private repos and have some public ones on GitHub.</p>
+                  <p>I use BitBucket to hold private repos and have some public ones hosted on <a href="https://github.com/codebubb">GitHub</a>.</p>
                 </div>
               </div>
             </div>
@@ -154,7 +154,7 @@
                   <img src="<?php echo get_template_directory_uri(); ?>/img/mysql.png" alt="">
                 </div>
                 <div class="back">
-                  <p>I find that it's good to know the SQL statements that your ORM is using and MySQL/MariaDB is easy to use.  </p>
+                  <p>I find that it's good to know the SQL statements that your ORM is using and MySQL/MariaDB is a good product to learn from.</p>
                 </div>
               </div>
             </div>
@@ -193,7 +193,7 @@
                   <img src="<?php echo get_template_directory_uri(); ?>/img/mongodb.png" alt="">
                 </div>
                 <div class="back">
-                  <p>I find MongoDB to be easy to get along with, usually integrating with Mongoose for ODM.</p>
+                  <p>I find MongoDB to be easy to get along with, usually integrating it with Mongoose for ODM.</p>
                 </div>
               </div>
             </div>
@@ -206,7 +206,7 @@
                   <img src="<?php echo get_template_directory_uri(); ?>/img/rails.png" alt="">
                 </div>
                 <div class="back">
-                  <p>I've been a big fan of Rails and have learnt a lot about MVC and software development from building apps.</p>
+                  <p>I've been a big fan of Rails and have learnt a lot about MVC and software development from building apps using it.</p>
                 </div>
               </div>
             </div>
@@ -233,7 +233,7 @@
                   <img src="<?php echo get_template_directory_uri(); ?>/img/android.png" alt="">
                 </div>
                 <div class="back">
-                  <p>I want to take the APIs from some of the projects I have created and build native apps that use these. </p>
+                  <p>I want to take the APIs from some of the projects I have created and build native apps that make use of these. </p>
                 </div>
               </div>
             </div>
@@ -266,18 +266,16 @@
       </div>
     </section>
     <section id="projects">
-      <!-- Thanks to https://davidwalsh.name/css-flip for flip effect -->
       <div class="container">
         <h2>Projects</h2>
         <ul>
           <li>
             <h3>Dad belly</h3>
             <div class="view view-third">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/projects/dadbelly.png" alt="">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/projects/dadbelly.png" alt="Dad Belly">
               <div class="mask">
-                   <!-- <h3>Dad Belly</h3> -->
-                   <p>An app for dads to track and lose weight</p>
-                       <a href="#" class="info">Visit Site</a>
+                   <p>An app writen using the MEAN stack for dads to count calories and track their weight loss.</p>
+                       <a href="http://dadbelly.com" class="info">Visit Site</a>
                    </div>
               </div>
 
@@ -285,60 +283,55 @@
           <li>
             <h3>Online Bingo Fans</h3>
             <div class="view view-third">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/projects/onlinebingofans.png" alt="">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/projects/onlinebingofans.png" alt="Online Bingo Fans">
               <div class="mask">
-                   <!-- <h3>Dad Belly</h3> -->
-                   <p>Finds the latest bingo deals</p>
-                       <a href="#" class="info">Visit Site</a>
+                   <p>Allows users to browse and search through an aggregated list of promotions from several different online bingo sites.</p>
+                       <a href="https://onlinebingofans.co.uk" class="info">Visit Site</a>
                    </div>
               </div>
           </li>
           <li>
             <h3>Quick API</h3>
             <div class="view view-third">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/projects/quickapi.png" alt="">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/projects/quickapi.png" alt="Quick API">
               <div class="mask">
-                   <!-- <h3>Dad Belly</h3> -->
-                   <p>Generate an API quickly directly from an SQLite database</p>
-                       <a href="#" class="info">View GitHub Repo</a>
+                   <p>A developer's  tool that generates an API directly from an SQLite database for providing test data.</p>
+                       <a href="https://github.com/codebubb/quickapi" class="info">View GitHub Repo</a>
                    </div>
               </div>
           </li>
           <li>
             <h3>SQL Injection Challenge</h3>
             <div class="view view-third">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/projects/sqlinjection.png" alt="">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/projects/sqlinjection.png" alt="SQL Injection Example">
               <div class="mask">
-                   <!-- <h3>Dad Belly</h3> -->
-                   <p>Practice SQL injection techniques on a simple login page</p>
-                       <a href="#" class="info">View GitHub Repo</a>
+                   <p>A quick way to spin up a login page where ethical hackers can practice SQL injection techniques.</p>
+                       <a href="https://github.com/codebubb/sqlinjectionexample" class="info">View GitHub Repo</a>
                    </div>
               </div>
           </li>
           <li>
             <h3>IT Jobs Board</h3>
             <div class="view view-third">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/projects/itjobs.png" alt="">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/projects/itjobs.png" alt="IT Jobs Board">
               <div class="mask">
-                   <!-- <h3>Dad Belly</h3> -->
-                   <p>A simple job posting app built with Rails.</p>
-                       <a href="#" class="info">Visit Site</a>
+                   <p>A jobs board app written in Rails that allows jobs to be created with multiple skills applied to them.</p>
+                       <a href="https://it-jobs.herokuapp.com" class="info">Visit Site</a>
                    </div>
               </div>
           </li>
           <li>
             <h3>JS Fundamentals</h3>
             <div class="view view-third">
-              <img src="<?php echo get_template_directory_uri(); ?>/img/projects/jsfundamentals.png" alt="">
+              <img src="<?php echo get_template_directory_uri(); ?>/img/projects/jsfundamentals.png" alt="JavaScript fundamentals">
               <div class="mask">
-                   <!-- <h3>Dad Belly</h3> -->
-                   <p>An online presentation for learning the basics of JavaScript</p>
-                       <a href="#" class="info">Visit Site</a>
+                <p>An online presentation for learning the basics of JavaScript.  Written using the Reveal.js presentation framework.</p>
+                       <a href="https://codbubb.github.io" class="info">Visit Site</a>
                    </div>
               </div>
           </li>
         </ul>
-        <h3 style="text-align: center;">For more examples take a look at my <a href="">GitHub</a> account.</h3>
+        <h3 style="text-align: center;">For more examples take a look at my <a href="https://github.com/codebubb">GitHub</a> account.</h3>
       </section>
     </div>
     <section id="blog">
